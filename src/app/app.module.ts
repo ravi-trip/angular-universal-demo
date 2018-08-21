fb84f6e59916ad69da388500c7aa74ae37975006
unknown <eshopbox@SSPL-16.positek.net>
2018-08-21 17:13:00 +0530

    last change use hash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const APP_ID = 'ang-uni-fire';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: APP_ID})
+    BrowserModule.withServerTransition({appId: APP_ID}),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,6 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 
+
 @NgModule({
   imports: [
     ServerModule,
